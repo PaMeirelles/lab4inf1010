8a9009d1dea0bb77d0689caa3904927558fd9224
ricleta <47068518+ricleta@users.noreply.github.com>
2022-06-14 03:11:39 +0000

    Refatoração do código da main para facilitar testes e melhorar organização. Criação do relatório e testes da função de inserção

--- main.c
+++ main.c
@@ -58,6 +58,7 @@ int placa_para_int(char * placa)
 int insere_hash(int * vetor, int key, int (* f)(int, int, int *, int), int k, int magic_number)
 {
   int i = f(key, k, vetor, magic_number);
+  
   if(i != -1)
   {
     vetor[i] = key;
@@ -72,7 +73,7 @@ int insere_hash(int * vetor, int key, int (* f)(int, int, int *, int), int k, in
 
 int simple_division(int key, int k, int * vetor, int magic_number)
 {
-  int i = ((magic_number * key + k) % 1033 );
+  int i = ((magic_number * key + (int)pow(k,2)) % 1033 );
   
   if(vetor[i] != -1)
   {
@@ -82,28 +83,44 @@ int simple_division(int key, int k, int * vetor, int magic_number)
   return i;
 }
 
-void testa_func(int(*f)(int, int, int*, int), int * vetor, int magic_number, int * colisoes, int * keys)
+int dobra(int key, int k, int * vetor, int magic_number)
+{
+  int p1 = key & 0x000003FF;
+  int p2 = key & 0x000FFC00;
+  int i = (p1 ^ p2 + magic_number *(int)pow(k, 2)) % 1033;
+
+  if(vetor[i] != -1)
+  {
+    return -1;
+  }
+
+  return i;
+}
+
+void testa_func(int(*f)(int, int, int*, int), int * vetor, int magic_number, int * colisoes, int * keys, int size)
 {
   int collisions = 0;
   clock_t start;
   clock_t stop;
   
   start = clock();
-  for(int i=0; i < 512; i++)
+  
+  for(int i=0; i < size; i++)
   {
-   collisions += insere_hash(vetor, keys[i], simple_division, 0,magic_number);
+   collisions += insere_hash(vetor, keys[i], f, 0,magic_number);
   }
   
   stop = clock();
   
   *colisoes = collisions;
-  }
-void bateria_de_testes(long tempo_limite, int(*f)(int, int, int*, int), int * vetor, int num_testes)
+}
+
+void bateria_de_testes(long tempo_limite, int(*f)(int, int, int*, int), int * vetor, int num_testes, int size_vetor)
 {
   char ** placas;
-  int * keys = malloc(512 * sizeof(int));
+  int * keys = malloc(size_vetor * sizeof(int));
 
-  int magic_number = 1;
+  int magic_number = 6;
   int * melhores_colisoes = malloc(4 * num_testes);
 
   for(int i=0; i<num_testes; i++)
@@ -113,26 +130,36 @@ void bateria_de_testes(long tempo_limite, int(*f)(int, int, int*, int), int * ve
   
   int coli;
   clock_t start = clock();
-  while((clock() - start) < tempo_limite){
-    for(int t=0; t<num_testes; t++){  
-      placas = gera_placas(512);
-      for(int i=0; i<512; i++){
-        keys[i] = placa_para_int(placas[i]);
+  
+  while((clock() - start) < tempo_limite)
+  {
+    for(int t=0; t<num_testes; t++)
+    {  
+      placas = gera_placas(size_vetor);
+      
+      for(int j=0; j<size_vetor; j++)
+      {
+        keys[j] = placa_para_int(placas[j]);
       }
-    testa_func(f, vetor, magic_number, &coli, keys);
-    melhores_colisoes[magic_number - 1] += coli;
-    for(int i=0; i<1033; i++)
-    {
-      vetor[i] = -1;
-    }
-    }
+    
+      testa_func(f, vetor, magic_number, &coli, keys, size_vetor);
+    
+      melhores_colisoes[magic_number - 1] += coli;
+    
+      for(int k=0; k<1033; k++)
+      {
+        vetor[k] = -1;
+      }
+  }
     printf("Finalizado teste para %d. Colisões restantes: %d. Tempo decorrido até o momento: %lds\n", magic_number, melhores_colisoes[magic_number-1]/num_testes, (clock()-start) / CLOCKS_PER_SEC);
     magic_number++;
   }
-  for(int i=0; i<magic_number; i++){
-    printf("Número mágico %d produziu em média %d colisões\n", i+1, melhores_colisoes[i]/num_testes);
+  
+  for(int n=0; n<magic_number; n++)
+  {
+    printf("Número mágico %d produziu em média %d colisões\n", n+1, melhores_colisoes[n]/num_testes);
   }
-  }
+}
 
 char ** gera_placas(int n)
 {
@@ -156,6 +183,9 @@ int main(void)
     vetor[i] = -1;
   }
   
-  bateria_de_testes(100000000, simple_division, vetor, 1000);
+  bateria_de_testes(100000000, dobra, vetor, 10000, 128);
+  // bateria_de_testes(100000000, dobra, vetor, 10000, 256);
+  // bateria_de_testes(100000000, dobra, vetor, 10000, 512);
+  
   return 0;
 }
